2551036c990406a4b4dffd387a586cc36241c904
Sam Sharma <samsharma.dev@gmail.com>
2024-07-23 21:41:16 -0700

    added browser router to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
