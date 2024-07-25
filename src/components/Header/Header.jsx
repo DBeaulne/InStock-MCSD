f9b1bc541a9fe7821165187edb21a40b305c7766
Daniel Beaulne <38220914+DBeaulne@users.noreply.github.com>
2024-07-25 12:50:29 -0400

    continue with header code build and styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,29 @@
-import React from "react";
 import "./Header.scss";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import logoImg from "../../assets/logo/InStock-Logo_1x.png";
 
 const Header = () => {
-  return <div>Header</div>;
+	return (
+		<div className="header">
+			<div className="header__logo">
+				{
+					<img
+						src={logoImg}
+						alt="InStock logo"
+					/>
+				}
+			</div>
+			<nav className="header__nav">
+				<div className="header__nav-link">
+					<NavLink to="warehouses">Warehouses</NavLink>
+				</div>
+				<div className="header__nav-link">
+					<NavLink to="inventory">Inventory</NavLink>
+				</div>
+			</nav>
+		</div>
+	);
 };
 
 export default Header;
