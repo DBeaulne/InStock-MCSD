7351ab7d4c2b31280be66de609343b372dd2eff1
Corey Bickford <cb6@live.ca>
2024-07-24 18:07:38 -0700

    Create initial footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+//Assets/Styles
+import "./Footer.scss";
+
+export default function Footer() {
+	return (
+		<div className='footer'>
+			<p className='footer__text'>
+				&copy InStock Inc. All Rights Reserved.
+			</p>
+		</div>
+	);
+}
