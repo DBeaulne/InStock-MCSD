ab552df3791c9c441d7aa12549b23356cb9abac9
Sam Sharma <samsharma.dev@gmail.com>
2024-07-25 20:56:34 -0700

    styled button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,7 +4,7 @@ import "./Button.scss";
 const Button = ({ className, type, text, txtClassName }) => {
   return (
     <button type={type} className={`btn ${className}`}>
-      {/* txtClassName is only for the Edit button to hide the text in mobile
+      {/* txtClassName is only for the Edit button to hide the text at mobile
       breakpoint */}
       <span className={`btn__txt ${txtClassName}`}>{text}</span>
     </button>
