dee3e399f6ec2845c9e851eb1f866e5401c14171
Corey Bickford <cb6@live.ca>
2024-07-27 08:09:12 -0700

    Progress commit

--- src/components/InventoryItemDetails/InventoryItemDetails.jsx
+++ src/components/InventoryItemDetails/InventoryItemDetails.jsx
@@ -2,26 +2,30 @@
 import "./InventoryItemDetails.scss";
 import backArrow from "../../assets/images/icons/arrow_back-24px.svg";
 import editIcon from "../../assets/icons/edit-24px.svg";
+//Components
+import Button from "../Button/Button";
 
 export default function InventoryItemDetails() {
 	return (
 		<div className='item-card__header'>
 			<div>
-				<button>
-					<img
-						src={backArrow}
-						alt='an arrow indicating back'
-					/>
-				</button>{" "}
+				<Button
+					className='button__back'
+					type=''
+					text=''
+					txtClassName=''
+				/>
+
 				<h1 className='item-card__item-name'>
 					{item.name}
 				</h1>
-				<button>
-					<img
-						src={editIcon}
-						alt='a simple image of a pencil'
-					/>
-				</button>
+
+				<Button
+					className='button__edit'
+					type=''
+					text=''
+					txtClassName=''
+				/>
 			</div>
 		</div>
 	);
