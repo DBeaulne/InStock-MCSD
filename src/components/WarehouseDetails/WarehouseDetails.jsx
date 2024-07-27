c3ab2b4889eef3c5b67ea83c3c50832cb28421d9
Sam Sharma <samsharma.dev@gmail.com>
2024-07-27 15:35:38 -0700

    added some styling to warehouse details component

--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -8,52 +8,69 @@ const WarehouseDetails = () => {
 
   return (
     <main className="warehouse-details">
-      <div className="warehouse-details__top">
-        <img src={ArrowBackIcon} alt="Back Icon" onClick={() => {}} />
-        <h2>
-          Washington
-          {/* {warehouseDetails.name} */}
-        </h2>
-        <Button
-          type="button"
-          text="Edit"
-          className="btn--prim btn--edit"
-          txtClassName="btn__txt--edit"
-        />
-      </div>
-      <div className="warehouse-details__mid">
-        <h3>Warehouse Address</h3>
-        <p>33 Pearl Street SW, Washington,USA</p>
-        <div className="warehouse-details__mid-container">
-          <div className="warehouse-details__name">
-            <h3>Contact Name</h3>
-            <p>
-              Graeme Lyon
-              {/* {warehouseDetails.manager} */}
-            </p>
-            <p>
-              Warehouse Manager
-              {/* {warehouseDetails.designation} */}
-            </p>
+      <div className="warehouse-details__container">
+        <div className="warehouse-details__top">
+          <img
+            className="warehouse-details__back-icon"
+            src={ArrowBackIcon}
+            alt="Back Icon"
+            onClick={() => {}}
+          />
+          <h2 className="warehouse-details__heading">
+            Washington
+            {/* {warehouseDetails.name} */}
+          </h2>
+          <Button
+            type="button"
+            text="Edit"
+            className="btn--prim btn--edit"
+            txtClassName="btn__txt--edit"
+          />
+        </div>
+        <div className="warehouse-details__mid">
+          <div className="warehouse-details__mid-container1">
+            <h3 className="warehouse-details__sub-heading">
+              Warehouse Address
+            </h3>
+            <p>33 Pearl Street SW, Washington,USA</p>
           </div>
-          <div className="warehouse-details__info">
-            <h3>Contact Information</h3>
-            <p>
-              +1 (647) 504-0911
-              {/* {warehouseDetails.phone} */}
-            </p>
-            <p>
-              glyon@instock.com
-              {/* {warehouseDetails.email} */}
-            </p>
+          <div className="warehouse-details__mid-container2">
+            <div className="warehouse-details__name">
+              <h3 className="warehouse-details__sub-heading">Contact Name</h3>
+              <p className="warehouse-details__txt">
+                Graeme Lyon
+                {/* {warehouseDetails.manager} */}
+              </p>
+              <p className="warehouse-details__txt">
+                Warehouse Manager
+                {/* {warehouseDetails.designation} */}
+              </p>
+            </div>
+            <div className="warehouse-details__info">
+              <h3 className="warehouse-details__sub-heading">
+                Contact Information
+              </h3>
+              <p className="warehouse-details__txt">
+                +1 (647) 504-0911
+                {/* {warehouseDetails.phone} */}
+              </p>
+              <p className="warehouse-details__txt">
+                glyon@instock.com
+                {/* {warehouseDetails.email} */}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      {/* <div className="warehouse-details__bottom">
-        {inventoryList.map((item) => (
+        <div className="warehouse-details__bottom">
+          {/* {inventoryList.map((item) => (
           <inventoryList />
-        ))}
-      </div> */}
+        ))} */}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil tempore
+          mollitia rem corrupti rerum numquam fugit odio fugiat alias nobis,
+          obcaecati sequi neque, autem doloremque aspernatur similique.
+          Voluptatum, quod blanditiis!
+        </div>
+      </div>
     </main>
   );
 };
