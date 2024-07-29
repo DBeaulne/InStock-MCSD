7593090c40c0f6465c7497d671816e3e588a1a54
Daniel <dbeaulne1@gmail.com>
2024-07-29 06:35:59 -0400

    complete modal window coding. Styling for mobile complete

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,17 +2,48 @@
 import React from "react";
 import "./Modal.scss";
 import Button from "../Button/Button";
+import closeIcon from "../../assets/icons/close-24px.svg";
 
-const Modal = () => {
+const Modal = ({ handleClose, location }) => {
+	location = "Washington";
+
 	return (
-		<div className="modal">
-			<h1>Delete Washington warehouse?</h1>
-			<p>
-				Please confirm that you'd like to delete the Washington warehouse from the list of warehouses. You won't be able
-				to undo this action.
-			</p>
-			<div className="modal__button-container">
-				<Button className={"btn--sec"}></Button>
+		<div className="modal-backdrop">
+			<div className="modal">
+				<div className="modal__icon-container">
+					<div className="modal__icon">
+						<svg
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg">
+							<g>
+								<path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" />
+							</g>
+						</svg>
+					</div>
+				</div>
+				<h1 className="modal__location">Delete {location} warehouse?</h1>
+				<p className="modal__confirm-text">
+					Please confirm that you'd like to delete the Washington warehouse from the list of warehouses. You won't be
+					able to undo this action.
+				</p>
+				<div className="modal__button-container">
+					<div className="modal__button">
+						<Button
+							className="btn--sec"
+							type="button"
+							text="Cancel"
+							onClick={handleClose}></Button>
+					</div>
+					<div className="modal__button">
+						<Button
+							className="btn--del"
+							type="button"
+							text="Delete"></Button>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- src/components/WarehouseList/Warehouse.jsx
+++ src/components/WarehouseList/Warehouse.jsx
@@ -1,39 +1,58 @@
-import './WarehouseList.scss';
+import "./WarehouseList.scss";
+import Modal from "../Modal/Modal";
+import { useState } from "react";
 
-const Warehouse = ({name, number, email, location, address, deletewarehouse, editwarehouse}) => {
-    return (
-        <>
-        <div className="warehouse">
-            <div className="warehouse__wrapper warehouse__wrapper--left">
-                <div className='warehouse__wrapper--inner'>
-                    <section className='warehouse__section-wrapper'> 
-                        <h4 className='warehouse__sub-title'>WAREHOUSE</h4>
-                        <p className='warehouse__text warehouse__text--bold-blue-font'>{location}</p>
-                    </section>
-                    <section className='warehouse__section-wrapper'> 
-                        <h4 className='warehouse__sub-title'>ADDRESS</h4>
-                        <p className='warehouse__text'>{address}</p>
-                    </section>
-                </div>
-                <button onClick={editwarehouse} className='warehouse__button warehouse__button--delete'></button>
-            </div>
-            <div className="warehouse__wrapper warehouse__wrapper--right">
-                <div className='warehouse__wrapper--inner'>
-                    <section className='warehouse__section-wrapper'> 
-                        <h4 className='warehouse__sub-title'>CONTACT</h4>
-                        <p className='warehouse__text'>{name}</p>
-                    </section>
-                    <section className='warehouse__section-wrapper'> 
-                        <h4 className='warehouse__sub-title'>CONTACT INFORMATION</h4>
-                        <p className='warehouse__text'>{number}</p>
-                        <p className='warehouse__text'>{email}</p>
-                    </section>
-                </div>
-                <button onClick={deletewarehouse} className='warehouse__button warehouse__button--edit'></button>
-            </div>
-        </div>
-        </>
-    ) 
-}
+const Warehouse = ({ name, number, email, location, address, deletewarehouse, editwarehouse }) => {
+	const [showModal, setShowModal] = useState(false);
+
+	const handleClose = () => {
+		setShowModal(false);
+	};
+
+	return (
+		<>
+			<div className="warehouse">
+				<div className="warehouse__wrapper warehouse__wrapper--left">
+					<div className="warehouse__wrapper--inner">
+						<section className="warehouse__section-wrapper">
+							<h4 className="warehouse__sub-title">WAREHOUSE</h4>
+							<p className="warehouse__text warehouse__text--bold-blue-font">{location}</p>
+						</section>
+						<section className="warehouse__section-wrapper">
+							<h4 className="warehouse__sub-title">ADDRESS</h4>
+							<p className="warehouse__text">{address}</p>
+						</section>
+					</div>
+					<button
+						onClick={() => setShowModal(true)}
+						className="warehouse__button warehouse__button--delete"></button>
+				</div>
+				<div className="warehouse__wrapper warehouse__wrapper--right">
+					<div className="warehouse__wrapper--inner">
+						<section className="warehouse__section-wrapper">
+							<h4 className="warehouse__sub-title">CONTACT</h4>
+							<p className="warehouse__text">{name}</p>
+						</section>
+						<section className="warehouse__section-wrapper">
+							<h4 className="warehouse__sub-title">CONTACT INFORMATION</h4>
+							<p className="warehouse__text">{number}</p>
+							<p className="warehouse__text">{email}</p>
+						</section>
+					</div>
+					<button
+						onClick={editwarehouse}
+						className="warehouse__button warehouse__button--edit"></button>
+				</div>
+			</div>
+
+			{showModal && (
+				<Modal
+					handleClose={handleClose}
+					location={location}
+				/>
+			)}
+		</>
+	);
+};
 
 export default Warehouse;
