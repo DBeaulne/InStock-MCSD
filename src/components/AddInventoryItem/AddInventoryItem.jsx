35836c63392d61328e3ecd759948a6c9989d68a0
Sam Sharma <samsharma.dev@gmail.com>
2024-07-28 18:48:07 -0700

    added the heading part of thee component

--- src/components/AddInventoryItem/AddInventoryItem.jsx
+++ src/components/AddInventoryItem/AddInventoryItem.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./AddInventoryItem.scss";
-
+import ArrowBackIcon from "../../assets/icons/arrow_back-24px.svg";
+import { useNavigate } from "react-router-dom";
 const AddInventoryItem = () => {
-  return <div>AddInventoryItem</div>;
+  const navigate = useNavigate();
+  return (
+    <section className="add-inventory">
+      <div className="add-inventory__container">
+        <div className="add-inventory__top">
+          <img
+            className="add-inventory__back-icon"
+            src={ArrowBackIcon}
+            alt="Back Icon"
+            onClick={() => {
+              navigate(-1);
+            }}
+          />
+          <h1 className="add-inventory__heading">Add New Inventory Item</h1>
+        </div>
+        <div className="add-inventory__mid"></div>
+      </div>
+    </section>
+  );
 };
 
 export default AddInventoryItem;
