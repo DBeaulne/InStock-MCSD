4cf749303c1f5e2d70933038b8f7832344ad0b91
Sam Sharma <samsharma.dev@gmail.com>
2024-08-03 21:53:16 -0700

    added a route to edit inventory item

--- src/components/EditInventoryItem/EditInventoryItem.jsx
+++ src/components/EditInventoryItem/EditInventoryItem.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import "./EditInventoryItem.scss";
 
 const EditInventoryItem = () => {
-  return <div>EditInventoryItem</div>;
+  return (
+    <div className="add-inventory__container">
+      <div className="add-inventory__top">
+        <img
+          className="add-inventory__back-icon"
+          src={ArrowBackIcon}
+          alt="Back Icon"
+          onClick={() => {
+            navigate("/inventory");
+          }}
+        />
+        <h1 className="add-inventory__heading">Edit Inventory Item</h1>
+      </div>
+    </div>
+  );
 };
 
 export default EditInventoryItem;
