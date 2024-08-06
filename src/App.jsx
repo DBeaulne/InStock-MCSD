2d5d04a6dad54b13b2d987c22e6701d319c2b1cb
Corey Bickford <cb6@live.ca>
2024-08-06 03:08:16 -0700

    Add submit  handler, cancel-submit handler, useParams to get warehouse id

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
 					/>
 
 					<Route
-						path='/warehouses/:id/edit'
+						path='/warehouses/:id'
 						element={<EditWarehouse />}
 					/>
 
--- src/components/EditWarehouse/EditWarehouse.jsx
+++ src/components/EditWarehouse/EditWarehouse.jsx
@@ -2,8 +2,8 @@
 import axios from "axios";
 import "./EditWarehouse.scss";
 import { apiUrl } from "../../App";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 //Assets:
 import errorIcon from "../../assets/icons/error-24px.svg";
 import ArrowBackIcon from "../../assets/icons/arrow_back-24px.svg";
@@ -14,6 +14,9 @@ import Button from "../Button/Button";
 export default function EditWarehouse() {
 	//enable navigation:
 	const navigate = useNavigate();
+
+  //grab warehouse id:
+  const { id } = useParams();
 
 	//grab data from form and set to state:
 	const [formData, setFormData] = useState({
@@ -26,6 +29,9 @@ export default function EditWarehouse() {
 		contact_phone: "",
 		contact_email: "",
 	});
+
+  //unpack form values:
+  const { warehouse_name, address, city, country, contact_name, contact_position, contact_phone, contact_email } = formData;
 
 	//track any input errors:
 	const [errors, setErrors] = useState({
@@ -54,4 +60,71 @@ export default function EditWarehouse() {
 			[name]: false,
 		});
 	};
+
+  //form submission handler:
+  	const handleSubmit = async event => {
+      //prevent default actions:
+      event.preventDefault();
+
+      //check for input values:
+		  const newErrors = {
+			  //'newErrors' is true if value is empty:
+			  warehouse_name: !values.warehouse_name,
+		    address: !values.address,
+		    city: !values.city,
+		    country: !values.country,
+		    contact_name: !values.contact_name,
+		    contact_position: !values.contact_position,
+		    contact_phone: !values.contact_phone,
+		    contact_email: !values.contact_email,
+		  };
+
+      //log any errors in state:
+		  setErrors(newErrors);
+
+      //if there are no errors, continue:
+      if (!newErrors) {
+        //confirm that the user wants to make the changes:
+        const confirmSubmit = window.confirm("Make these changes to the current warehouse?");
+      }
+
+      //if changes are accepted, navigate back to warehouse list:
+      if (confirmSubmit) {
+        navigate(`/warehouses`);
+      }
+      
+      try {
+        await axios
+          //make changes to database entry:
+          .put(`${apiUrl}/warehouses/${id}`, formData)
+          //revert form data:
+          .then(() => {
+            setFormData({
+              warehouse_name: "",
+              address: "",
+              city: "",
+              country: "",
+              contact_name: "",
+              contact_position: "",
+              contact_phone: "",
+              contact_email: "",
+            });
+          });
+
+      } catch (error) {
+        console.log("Encountered an error while attempting to edit the warehouse:", error.message);
+      }
+	};
+
+  //discard-changes handler:
+  const handleDiscardChanges = async () => {
+		const cancelConfirmation = window.confirm(
+			"Discard changes to the current warehouse?"
+		);
+		if (cancelConfirmation) {
+			navigate(`/warehouses`);
+		}
+	};
+
+  return ();
 }
