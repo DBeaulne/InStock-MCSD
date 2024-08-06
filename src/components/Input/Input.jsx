b59bc6a7dd59723bc18e505cc0ce7b0bc9798bee
Daniel <dbeaulne1@gmail.com>
2024-08-06 15:04:27 -0400

    add phone number and email validation logic. Fixed header styling for button container

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,80 +1,70 @@
 import "./Input.scss";
 import { useState } from "react";
-import errorIcon from "../../assets/icons/error-24px.svg";
 
-const Input = ({
-  classname,
-  placeholder,
-  name,
-  type,
-  value,
-  txtArea,
-  search,
-  onChange,
-}) => {
-  const [focus, setFocus] = useState("");
-  const [error, setError] = useState("");
+const Input = ({ classname, placeholder, name, type, value, txtArea, search, onChange }) => {
+	const [focus, setFocus] = useState("");
+	const [error, setError] = useState("");
 
-  const onInvalid = () => {
-    setError("site_input--error");
-    setFocus("");
-  };
+	const onInvalid = () => {
+		setError("site_input--error");
+		setFocus("");
+	};
 
-  const onFocus = () => {
-    setFocus("site_input--active");
-  };
+	const onFocus = () => {
+		setFocus("site_input--active");
+	};
 
-  const onBlur = () => {
-    setFocus("");
-    setError("");
-  };
+	const onBlur = () => {
+		setFocus("");
+		setError("");
+	};
 
-  if (txtArea) {
-    return (
-      <textarea
-        onInvalid={onInvalid}
-        onFocus={onFocus}
-        onBlur={onBlur}
-        required
-        value={value}
-        className={`site_input site_input--txtArea ${classname} ${focus} ${error}`}
-        placeholder={placeholder}
-        type="text"
-        name={name}
-        onChange={onChange}
-      />
-    );
-  } else if (search) {
-    return (
-      <input
-        onInvalid={onInvalid}
-        onFocus={onFocus}
-        onBlur={onBlur}
-        required
-        value={value}
-        className={`site_input ${classname} ${focus} ${error}`}
-        placeholder={placeholder}
-        type="text"
-        name={name}
-        onChange={onChange}
-      />
-    );
-  } else {
-    return (
-      <input
-        onInvalid={onInvalid}
-        onFocus={onFocus}
-        onBlur={onBlur}
-        required
-        value={value}
-        className={`site_input ${classname} ${focus} ${error}`}
-        placeholder={placeholder}
-        type={type}
-        name={name}
-        onChange={onChange}
-      />
-    );
-  }
+	if (txtArea) {
+		return (
+			<textarea
+				onInvalid={onInvalid}
+				onFocus={onFocus}
+				onBlur={onBlur}
+				required
+				value={value}
+				className={`site_input site_input--txtArea ${classname} ${focus} ${error}`}
+				placeholder={placeholder}
+				type="text"
+				name={name}
+				onChange={onChange}
+			/>
+		);
+	} else if (search) {
+		return (
+			<input
+				onInvalid={onInvalid}
+				onFocus={onFocus}
+				onBlur={onBlur}
+				required
+				value={value}
+				className={`site_input ${classname} ${focus} ${error}`}
+				placeholder={placeholder}
+				type="text"
+				name={name}
+				onChange={onChange}
+			/>
+		);
+	} else {
+		return (
+			<input
+				onInvalid={onInvalid}
+				onFocus={onFocus}
+				onBlur={onBlur}
+				required
+				value={value}
+				className={`site_input ${classname} ${focus} ${error}`}
+				placeholder={placeholder}
+				type={type}
+				name={name}
+				onChange={onChange}
+			/>
+		);
+	}
 };
 
 export default Input;
