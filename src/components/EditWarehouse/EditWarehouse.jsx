e24fdc45d308d0db2ee60c7054074efd8d434c0f
Corey Bickford <cb6@live.ca>
2024-08-06 02:32:13 -0700

    Add error state and input handler

--- src/components/EditWarehouse/EditWarehouse.jsx
+++ src/components/EditWarehouse/EditWarehouse.jsx
@@ -14,7 +14,8 @@ import Button from "../Button/Button";
 export default function EditWarehouse() {
 	//enable navigation:
 	const navigate = useNavigate();
-	//grab data from form:
+
+	//grab data from form and set to state:
 	const [formData, setFormData] = useState({
 		warehouse_name: "",
 		address: "",
@@ -25,4 +26,32 @@ export default function EditWarehouse() {
 		contact_phone: "",
 		contact_email: "",
 	});
+
+	//track any input errors:
+	const [errors, setErrors] = useState({
+		warehouse_name: false,
+		address: false,
+		city: false,
+		country: false,
+		contact_name: false,
+		contact_position: false,
+		contact_phone: false,
+		contact_email: false,
+	});
+
+	//input handler:
+	const handleInputChange = event => {
+		const { name, value } = event.target;
+		//spread values back into state, changing only the modified value:
+		setValues({
+			...values,
+			[name]: value,
+		});
+
+		//clear error state when user starts typing:
+		setErrors({
+			...errors,
+			[name]: false,
+		});
+	};
 }
