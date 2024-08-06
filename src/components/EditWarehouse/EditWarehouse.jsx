fa283f41f4d45b764fa629d937556f66d411976d
Corey Bickford <cb6@live.ca>
2024-08-06 02:23:04 -0700

    Add imports, define form-state, enable navigation

--- src/components/EditWarehouse/EditWarehouse.jsx
+++ src/components/EditWarehouse/EditWarehouse.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+//Imports:
+import axios from "axios";
 import "./EditWarehouse.scss";
+import { apiUrl } from "../../App";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+//Assets:
+import errorIcon from "../../assets/icons/error-24px.svg";
+import ArrowBackIcon from "../../assets/icons/arrow_back-24px.svg";
+//Components:
+import Input from "../Input/Input";
+import Button from "../Button/Button";
 
-const EditWarehouse = () => {
-  return <div>EditWarehouse</div>;
-};
-
-export default EditWarehouse;
+export default function EditWarehouse() {
+	//enable navigation:
+	const navigate = useNavigate();
+	//grab data from form:
+	const [formData, setFormData] = useState({
+		warehouse_name: "",
+		address: "",
+		city: "",
+		country: "",
+		contact_name: "",
+		contact_position: "",
+		contact_phone: "",
+		contact_email: "",
+	});
+}
