9ef1fbc402e69363cff76869550dfc1063ee2eda
Daniel <dbeaulne1@gmail.com>
2024-08-06 21:02:08 -0400

    removed all un-neccessary global partial calls

--- src/components/DeleteInventoryItem/DeleteInventoryItem.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./DeleteInventoryItem.scss";
-
-const DeleteInventoryItem = () => {
-  return <div>DeleteInventoryItem</div>;
-};
-
-export default DeleteInventoryItem;
